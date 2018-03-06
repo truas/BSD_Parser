75c45a199f412f6f97710ac88b6e20388fd8c2c2
truas <33872126+truas@users.noreply.github.com>
2018-03-06 15:39:19 +0900

    absolute path for IO fixed

--- text_parser/text_operations/operation.py
+++ text_parser/text_operations/operation.py
@@ -3,14 +3,28 @@ Created on Mar 5, 2018
 
 @author: Terry Ruas
 '''
-#import-modules
+#import
+import os
+import sys
+
+#python module absolute path
+pydir_name = os.path.dirname(os.path.abspath(__file__))
+
+#python path definition
+sys.path.append(os.path.join(os.path.dirname(__file__), 'text_parser/text_operatinos'))
+
+#local-imports
 from text_operations import read_write as rw
 
-#input/output
-input_folder = 'C:/tmp_project/BSDParser/input'
-output_folder = 'C:/tmp_project/BSDParser/output'
+#relative input/output folders
+input_folder = '/tmp_project/BSDParser/input'
+output_folder = '/tmp_project/BSDParser/output'
 
-docs = rw.make_doc_list(input_folder)
-rw.process_one_file(docs, output_folder)
-#rw.process_many_files(docs, input_folder, output_folder) #in case you want one file per document
+#in/ou relative location
+in_fname = os.path.join(pydir_name, input_folder)
+ou_fname = os.path.join(pydir_name, output_folder)
+
+docs = rw.make_doc_list(in_fname)
+rw.process_one_file(docs, ou_fname)
+#rw.process_many_files(docs, in_fname, ou_fname) #in case you want one file per document
 print('Finished...')
